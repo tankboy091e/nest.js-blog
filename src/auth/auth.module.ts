dd5949583d789088ef980f5c10978e2c49b6653c
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-07-17 18:46:50 +0900

    Fix refresh token set-cookie

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,15 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
+import { SecretaryModule } from 'src/secretary/secretary.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), JwtModule.register({}), UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({}),
+    UserModule,
+    SecretaryModule,
+  ],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
